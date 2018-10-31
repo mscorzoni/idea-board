bafeae75e3a8325c292fd438d7ddbe6df5123c89
Marcos Scorzoni <mlessa@gmail.com>
2018-10-31 21:59:30 +0000

    add delete method to post

--- client/src/components/Idea.js
+++ client/src/components/Idea.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const Idea = (props) => {
-  return (
-    <div className="tile" key={props.idea.id}>
-      <h4>{props.idea.title}</h4>
-      <p>{props.idea.body}</p>
-    </div>
-  );
-};
+class Idea extends Component {
+  handleClick = () => { this.props.onClick(this.props.idea.id) }
 
-export default Idea;
+  handleDelete = () => { this.props.onDelete(this.props.idea.id) }
+
+  render() {
+    return (
+      <div className="tile">
+        <span className="deleteButton" onClick={this.handleDelete}>x</span>
+        <h4 onClick={this.handleClick}>{this.props.idea.title}</h4>
+        <p onClick={this.handleClick}>{this.props.idea.body}</p>
+      </div>
+    )
+  }
+}
+
+export default Idea
