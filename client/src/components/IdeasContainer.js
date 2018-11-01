bee9a1c165b3609b744fe8816c5c0668a46b572c
Marcos Scorzoni <mlessa@gmail.com>
2018-11-01 13:29:44 +0000

    implement form for idea

--- client/src/components/IdeasContainer.js
+++ client/src/components/IdeasContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import Idea from './Idea';
+
 import axios from 'axios';
+import update from 'immutability-helper';
+
+import Idea from './Idea';
+import IdeaForm from './IdeaForm';
+
 
 class IdeasContainer extends Component {
   state = {
-    ideas: []
+    ideas: [],
+    editingIdeaId: null
   }
 
   componentDidMount() {
@@ -20,6 +26,9 @@ class IdeasContainer extends Component {
     axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: ' ', body: ' '}})
       .then(response => {
         console.log(response)
+        const ideas = update(this.state.ideas, { $slice: [[0, 0, response.data]]
+        })
+        this.setState({ideas: ideas, editingIdeaId: response.data.id})
       })
       .catch(error => console.log(error))
   }
@@ -29,9 +38,14 @@ class IdeasContainer extends Component {
       <div>
         <button className="newIdeaButton" onClick={this.addNewIdea}>New Idea</button>
         {this.state.ideas.map((idea) => {
-          return (
-            <Idea idea={idea} key={idea.id} />
-          )
+          if(this.state.editingIdeaId === idea.id) {
+            return(<IdeaForm idea={idea} key={idea.id} />)
+          }
+          else {
+            return (
+              <Idea idea={idea} key={idea.id} />
+            )
+          }
         })}
       </div>
     );
