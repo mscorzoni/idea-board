97dda7edec52d62ded4f3f5e8a12342fe60b0b61
Marcos Scorzoni <mlessa@gmail.com>
2018-11-01 11:49:36 +0000

    add post server to the idea

--- client/src/components/IdeasContainer.js
+++ client/src/components/IdeasContainer.js
@@ -16,9 +16,18 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewIdea = () => {
+    axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: ' ', body: ' '}})
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
+        <button className="newIdeaButton" onClick={this.addNewIdea}>New Idea</button>
         {this.state.ideas.map((idea) => {
           return (
             <Idea idea={idea} key={idea.id} />
